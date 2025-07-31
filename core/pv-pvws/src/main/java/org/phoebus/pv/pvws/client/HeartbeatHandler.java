90598d8b62335150c78b6a05cf3c58207a5f0b36
gbonilla <georgebonillla@gmail.com>
2025-07-31 16:25:13 -0400

    Added heart and reconnect handlers❤️

---
+++ core/pv-pvws/src/main/java/org/phoebus/pv/pvws/client/HeartbeatHandler.java
@@ -0,0 +1,78 @@
+package org.phoebus.pv.pvws.client;
+
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+public class HeartbeatHandler {
+
+    private final PVWS_Client client;
+    private ScheduledExecutorService scheduler;
+
+    private ScheduledFuture<?> heartbeatTask;
+    private volatile long lastPongTime;
+
+    private final long heartbeatInterval;
+    private final long heartbeatTimeout;
+
+
+    public HeartbeatHandler(PVWS_Client client,
+                            ScheduledExecutorService scheduler,
+                            long heartbeatInterval,
+                            long heartbeatTimeout) {
+        this.client = client;
+        this.scheduler = scheduler;
+        this.heartbeatInterval = heartbeatInterval;
+        this.heartbeatTimeout = heartbeatTimeout;
+    }
+
+
+
+    public void start(){
+
+
+        heartbeatTask = scheduler.scheduleAtFixedRate(() -> {
+            try {
+                //System.out.println(" Heartbeat loop running");
+                client.sendPing();
+                System.out.println("Ping sent");
+                scheduler.schedule(() -> {
+                    if (System.currentTimeMillis() - lastPongTime > getHeartbeatTimeout()) {
+                        System.out.println("Heartbeat timeout. Reconnecting...");
+                        client.attemptReconnect();
+                    }
+                }, 3, TimeUnit.SECONDS);
+            } catch (Exception e) {
+                System.out.println("Heartbeat error: " + e.getMessage());
+                client.attemptReconnect();
+            }
+        }, 0, heartbeatInterval, TimeUnit.MILLISECONDS);
+
+    }
+
+    public void stop(){
+
+        if (heartbeatTask != null && !heartbeatTask.isCancelled()) {
+            heartbeatTask.cancel(true);
+        }
+
+
+    }
+
+    public void setLastPongTime(Long lastPongTime)
+    {
+        this.lastPongTime = lastPongTime;
+    }
+
+
+    public long getHeartbeatInterval() {
+        return heartbeatInterval;
+    }
+
+    public long getHeartbeatTimeout() {
+        return heartbeatTimeout;
+    }
+
+}
